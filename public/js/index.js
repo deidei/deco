0cbe18149cc5efae4c5bddb9ce90ee8f0a389bef
Feng-Zihao <feng.zh.zq@gmail.com>
2013-12-13 18:12:52 +0800

    Setup angularJS. Setup tryLogin();

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,29 @@
-function loginCtrl($scope) {
-    $scope.loginHint= 'hello';
+function loginCtrl($scope, $http) {
+
+    $scope.loginHint = 'hello';
+    $scope.loginName = '';
+    $scope.loginPassword = '';
+
+
+    $scope.tryLogin = function(ev) {
+        
+        console.log(ev);
+        if (ev.which != 13) {
+            return;
+        }
+
+        var data = {'user': $scope.loginName,
+                    'password': $scope.loginPassword};
+
+        console.log(data);
+        $http.post('/login', data).
+            success(function(data, status, headers, config) {
+                $scope.loginHint = "success";
+            }).
+            error(function(data, status, headers, config) {
+                $scope.loginHint = "fail";
+            });
+
+    };
 };
 
