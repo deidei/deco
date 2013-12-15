893308dfdbc784d3c71c4cfc78820a66692bac9d
Liu Yang <liuyang1204@hotmail.com>
2013-12-16 01:08:15 +0800

    Add functions in server side

--- decoapp.rb
+++ decoapp.rb
@@ -11,11 +11,12 @@ class DecoApp < Sinatra::Application
 
     def login_required!
         if session[:user].nil?
-            halt 401, "Not authorized\n"
+            redirect "/login.html"
         end
     end
 
     before do
+        login_required! unless request.path_info == "/login"
         body = request.body.read
         @json = JSON.parse(body) unless body.empty?
     end
@@ -33,8 +34,7 @@ class DecoApp < Sinatra::Application
     end
 
     get "/" do
-        login_required!
-        "Hello"
+        redirect "/index.html"
     end
 
     post "/users" do
@@ -45,12 +45,56 @@ class DecoApp < Sinatra::Application
     end
 
     get "/users" do
-        login_required!
         User.all.to_json
     end
 
     get "/daily_reports" do
         DailyReport.all.to_json
+    end
+
+    get "/daily_reports/:date" do
+        if params[:date] == "today"
+            date = Date.today
+        else
+            date = Date.parse(params[:date])
+        end
+        report = DailyReport.find_by(user_id: session[:user], date: date)
+        if report.nil?
+            report = DailyReport.new
+            report.user_id = session[:user]
+            report.date = date
+            report.status = "Not created"
+            report.save!
+        end
+        report.to_json
+    end
+
+    post "/daily_reports" do
+        p @json
+        date = @json["date"]
+        report = DailyReport.find_by(user_id: session[:user], date: date)
+        if report.nil?
+            report = DailyReport.new(@json)
+            report.status = "Draft"
+            report.save!
+            report.to_json
+        elsif report.status == "Published"
+            status 403
+        else
+            report.assign_attributes(@json)
+            report.status = "Draft"
+            report.save
+            report.to_json
+        end
+    end
+
+    get "/daily_reports/today/status" do
+        report = DailyReport.find_by(user_id: session[:user], date: Date.today)
+        if report.nil?
+            "Not created"
+        else
+            report.status
+        end
     end
 
 end
